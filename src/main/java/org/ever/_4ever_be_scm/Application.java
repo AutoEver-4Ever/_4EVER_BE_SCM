f8adc444a7d3e410a75d0fbfce0d1e3db9b47df7
Hojip-Kim <rlaghwlq12@naver.com>
2025-09-30 12:23:54 +0900

    init : SCM Service init

---
+++ src/main/java/org/ever/_4ever_be_scm/Application.java
@@ -0,0 +1,13 @@
+package org.ever._4ever_be_scm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/ever/_4ever_be_scm/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ever._4ever_be_scm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
