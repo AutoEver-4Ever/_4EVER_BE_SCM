c0f5e07fb4b9cd2b7f787a19f0ff83dd580d1b5b
DonjJin-github <cdj7235@naver.com>
2025-10-27 17:03:55 +0900

    feat(mm): 구매관리 컨트롤러, vo 추가(#12)

---
+++ src/main/java/org/ever/_4ever_be_scm/scm/mm/vo/SupplierSearchVo.java
@@ -0,0 +1,17 @@
+package org.ever._4ever_be_scm.scm.mm.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SupplierSearchVo {
+    private String statusCode;
+    private String category;
+    private int page;
+    private int size;
+}
